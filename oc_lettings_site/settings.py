d19817e0447ebc321a17a41c0b3e7bb74f199f20
l0mens <lomens@hotmail.fr>
2023-04-29 09:23:24 +0200

    Adding Sentry

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -1,4 +1,6 @@
 import os
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -15,6 +17,21 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
+sentry_sdk.init(
+    dsn="https://16fc105abe474ef19bb43627a63139a4@o4505096214020096.ingest.sentry.io/4505096215265280",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 # Application definition
 
